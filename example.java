2167863d2ad3b7cbfdd9dd6b98ebeee0da885489
rania <117826682+raniagomaahossny@users.noreply.github.com>
2023-02-20 15:26:19 -0800

    lab1/finished

---
+++ example.java
@@ -0,0 +1,20 @@
+public class Example {
+public static void main(String args[]){
+System.out.println("this is a simple java program");
+}
+}
+
+
+
+
+public class Example2{
+public static void main(String args[]){
+if (args.length >= 0){
+if (args[0].equals("CoreJava")){
+System.out.println("True value" +args[0]);
+}
+}
+}
+}
+
+
