ab5ce1b36af5a6f0194cf63fbed72c8d6174c406
rania <117826682+raniagomaahossny@users.noreply.github.com>
2023-02-24 02:27:03 -0800

    Add files via upload

---
+++ lab6-/src/AppCompatActivity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+class AppCompatActivity {
+
+    void onCreate(Bundle savedInstanceState) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
